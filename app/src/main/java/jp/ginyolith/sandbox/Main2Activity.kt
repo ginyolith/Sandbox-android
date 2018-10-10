84504e0d42d01ba75a10fdf5ff246fa8b6bf5d76
Fumiya Akagi <fumiya.akagi@iridge.jp>
2018-10-10 20:14:57 +0900

    implemented

--- app/src/main/java/jp/ginyolith/sandbox/Main2Activity.kt
+++ app/src/main/java/jp/ginyolith/sandbox/Main2Activity.kt
@@ -3,10 +3,10 @@ package jp.ginyolith.sandbox
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
     }
 }
